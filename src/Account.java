4125128652731b92ff1e238cd1e9906055282b69
Dmitry K <sieger1012@gmail.com>
2026-02-18 10:27:05 +0700

    Перевод класса Account в абстрактный + добавлены три метода: Оплата, Пополнение и Перевод.

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,29 @@
-public class Account {
+public abstract class Account {
+    protected int balance;
+
+    public Account(int balance){
+        this.balance = balance;
+    }
+
+    public  int getBalance(){
+        return balance;
+    }
+
+    // Оплата
+    public abstract boolean pay(int amount);
+
+    // Пополнение
+    public abstract boolean addMoney(int amount);
+
+    // Перевод
+    public boolean transfer(Account account, int amount) {
+        if (this.pay(amount)) {
+            account.addMoney(amount);
+            System.out.println("Перевод выполнен: " + amount);
+            return true;
+        }
+
+        System.out.println("Перевод невозможен");
+        return false;
+    }
 }
